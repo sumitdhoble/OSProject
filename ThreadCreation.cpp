a511c5381006e1730dd8133dac6f8f01c2054d27
sumit <sumit@sumit-VirtualBox.(none)>
2012-10-14 11:57:04 -0700

    more iterationsfor thread creation and runnning

--- ThreadCreation.cpp
+++ ThreadCreation.cpp
@@ -9,7 +9,7 @@ void doNothing();
 
 int main(){
 	int i,j,k;
-	int iterations = 100, sum = 0 , avg = 0, time = 0;
+	int iterations = 10000, sum = 0 , avg = 0, time = 0;
 	struct timeval startTime, endTime;
 	pthread_t thread;
 
@@ -17,6 +17,7 @@ int main(){
 		gettimeofday(&startTime, NULL);	
 		pthread_create(&thread, NULL, (void *(*)(void *))doNothing, 0);
 		gettimeofday(&endTime, NULL);
+		pthread_join(thread, NULL);
 		time = (endTime.tv_sec - startTime.tv_sec)*1000000 + (endTime.tv_usec - startTime.tv_usec);
 		cout << time << endl;
 		sum += time;
