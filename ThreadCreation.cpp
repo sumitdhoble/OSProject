f5a41f7ca06b58efb37914757c86c3c922601357
sumit <sumit@sumit-VirtualBox.(none)>
2012-10-14 11:47:48 -0700

    Thread creation and running time recoreded.

---
+++ ThreadCreation.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<pthread.h>
+#include<sys/time.h>
+#include<unistd.h>
+
+using namespace std;
+
+void doNothing();
+
+int main(){
+	int i,j,k;
+	int iterations = 100, sum = 0 , avg = 0, time = 0;
+	struct timeval startTime, endTime;
+	pthread_t thread;
+
+	for(i = 0 ; i < iterations ; i++){
+		gettimeofday(&startTime, NULL);	
+		pthread_create(&thread, NULL, (void *(*)(void *))doNothing, 0);
+		gettimeofday(&endTime, NULL);
+		time = (endTime.tv_sec - startTime.tv_sec)*1000000 + (endTime.tv_usec - startTime.tv_usec);
+		cout << time << endl;
+		sum += time;
+	}	
+
+	avg = sum/iterations;
+	cout << "Average : " << avg << endl;
+	return 0;
+}
+
+void doNothing(){
+}
