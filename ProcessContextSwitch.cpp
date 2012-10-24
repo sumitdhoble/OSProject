e9fc922b3b8d921cb69e5c912b6df296adb54bd6
sumit <sumit@sumit-VirtualBox.(none)>
2012-10-23 20:13:21 -0700

    All timestamps are now written to separate files; we can generate the context switch time from these files; no need to take into account the time needed for file operations

--- ProcessContextSwitch.cpp
+++ ProcessContextSwitch.cpp
@@ -7,9 +7,6 @@
 
 using namespace std;
 
-
-
-
 int main(){
 
 	int i,j,k;
@@ -17,74 +14,74 @@ int main(){
 	char buf;
 	int pipePC[2];
 	int pipeCP[2];
+	FILE *fp;
 
 	 if (pipe(pipePC) == -1 || pipe(pipeCP) == -1) {
       exit(EXIT_FAILURE);
   }
 
+	fp = (FILE *)fopen("ContextSwitch1StartTime.txt", "w");
+	fp = (FILE *)fopen("ContextSwitch1EndTime.txt", "w");
+	fp = (FILE *)fopen("ContextSwitch2StartTime.txt", "w");
+	fp = (FILE *)fopen("ContextSwitch2EndTime.txt", "w");
+
 	pid = fork();
-	
+
 	if(pid == 0){
-		int i = iterations, t1 = 0;
-		FILE *fp;
+		int i = iterations, t1 = 0;		
 		struct timeval startTime, endTime, startTime2, endTime2;
 
 		while(i > 0){
-			
+git			
 			close(pipePC[1]);
 			read(pipePC[0], &buf, 1);
 			gettimeofday(&endTime, NULL);
 			close(pipePC[0]);
-
-			fp = (FILE *)fopen("fileParent.txt", "r");
-			fscanf(fp,"%ld %ld", &startTime.tv_sec, &startTime.tv_usec);
+			fp = (FILE *)fopen("ContextSwitch1EndTime.txt", "a");
+			fprintf(fp,"%ld\t%ld\n", endTime.tv_sec, endTime.tv_usec);
 			fclose(fp);
-			t1 += (endTime.tv_sec - startTime.tv_sec)*1000000 + (endTime.tv_usec - startTime.tv_usec);
 
 			close(pipeCP[0]);
 			gettimeofday(&startTime2, NULL);
-			fp = (FILE *)fopen("fileChild.txt", "w");
-			fprintf(fp,"%ld %ld", startTime2.tv_sec, startTime2.tv_usec);
-			fclose(fp);
 			write(pipeCP[1], "h" , 1);
+			fp = (FILE *)fopen("ContextSwitch2StartTime.txt", "a");
+			fprintf(fp,"%ld\t%ld\n", startTime2.tv_sec, startTime2.tv_usec);
+			fclose(fp);
+
 			close(pipeCP[1]);
 
 			i--;
 		}
 
-		cout << "t1\t" << (float)t1 / iterations << endl;
 		exit(0);
 	}
 	else {
 		int i = iterations, t2 = 0;
-		FILE *fp;
 		struct timeval startTime, endTime, startTime2, endTime2;
 
 		while(i > 0){
 
 			close(pipePC[0]);          
 			gettimeofday(&startTime, NULL);
-			fp = (FILE *)fopen("fileParent.txt", "w");
-			fprintf(fp,"%ld %ld", startTime.tv_sec, startTime.tv_usec);
-			fclose(fp);
-		  write(pipePC[1], "w" , 1);
+			write(pipePC[1], "w" , 1);
+			fp = (FILE *)fopen("ContextSwitch1StartTime.txt", "a");
+			fprintf(fp,"%ld\t%ld\n", startTime.tv_sec, startTime.tv_usec);
+			fclose(fp);		  
 			close(pipePC[1]);
 			
 			
 			close(pipeCP[1]);
 			read(pipeCP[0], &buf, 1);
 			gettimeofday(&endTime2, NULL);
-			fp = (FILE *)fopen("fileChild.txt", "r");
-			fscanf(fp,"%ld %ld", &startTime2.tv_sec, &startTime2.tv_usec);
+			fp = (FILE *)fopen("ContextSwitch2EndTime.txt", "a");
+			fprintf(fp,"%ld\t%ld\n", endTime2.tv_sec, endTime2.tv_usec);
 			fclose(fp);
 			close(pipeCP[0]);
-			t2 += (endTime2.tv_sec - startTime2.tv_sec)*1000000 + (endTime2.tv_usec - startTime2.tv_usec);
 
 			i--;
 		}
 
     wait(NULL);                /* Wait for child */
-		cout << "t2\t" << (float)t2 / iterations << endl;
     exit(0);
 	}
 
