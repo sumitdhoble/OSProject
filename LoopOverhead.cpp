890e468c4d789f89a041d2a16da14fa6fdea1c3a
sumit <sumit@sumit-VirtualBox.(none)>
2012-10-27 16:42:15 -0700

    Better way to measure loop overhead

--- LoopOverhead.cpp
+++ LoopOverhead.cpp
@@ -15,19 +15,25 @@ int main(){
 	int iterations = 100000000;
 	int measurements = 100;
 	struct timeval startTime, endTime, testTime;
-	int runtime, sum = 0;
+	int runtime;
+	float measurementResult = 0.0, finalResult = 0.0, sum = 0.0;
+
+	cout << "hello" << endl;
 
 	for (i = 0; i < measurements; i++) {
 		gettimeofday(&startTime, NULL);
 		for (j = 0; j < iterations; j++) {
 			//gettimeofday(&testTime, NULL);
-			doNothing(i, j, k, l, m, n, o, p);
+			//doNothing(i, j, k, l, m, n, o, p);
 		}
 		gettimeofday(&endTime, NULL);
 		runtime = (endTime.tv_sec - startTime.tv_sec)*1000000 + (endTime.tv_usec - startTime.tv_usec);
-		cout << runtime << endl;
-		sum += runtime;
+		measurementResult = (float)runtime/iterations;
+		cout << measurementResult << endl;
+		sum += measurementResult;
 	}
-	cout << sum/measurements << endl;
-	sum = 0;
+	finalResult = (float)sum/measurementResult;
+	cout << finalResult << endl;
+	
+	return 0;
 }
