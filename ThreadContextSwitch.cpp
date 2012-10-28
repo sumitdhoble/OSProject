7ce7c7d460a1f4b31cd7b56d6ff36b872247a81c
Sumit Dhoble <sumit@vdeshpan-XPS-8300.(none)>
2012-10-28 12:37:53 -0700

    Thread context switch time now measured using rdtsc()

--- ThreadContextSwitch.cpp
+++ ThreadContextSwitch.cpp
@@ -3,27 +3,31 @@
 #include<sys/time.h>
 #include<unistd.h>
 #include<sched.h>
+#include"rdtsc.h"
+
+#define CLOCK_RATE 3.4
+
 using namespace std;
 
 void doNothing();
-struct timeval startTime, endTime;
+long int startTime, endTime;
 int iterations = 1000000;
 int i = 0;
 
 int main(){
 	int j,k;
-	int sum = 0, totalTime = 0;
-	float time = 0;	
+	int sum = 0;
+	float totalTime = 0.0, time = 0.0;	
 	pthread_t thread;
 
 	pthread_create(&thread, NULL, (void *(*)(void *))doNothing, NULL);
-	gettimeofday(&startTime, NULL);	
+	startTime = rdtsc();
 	for(i = 0 ; i < iterations ; i++){
 		sched_yield();
 	}
-	gettimeofday(&endTime, NULL);
+	endTime = rdtsc();
 	pthread_join(thread, NULL);
-	totalTime = (endTime.tv_sec - startTime.tv_sec)*1000000 + (endTime.tv_usec - startTime.tv_usec);
+	totalTime = (endTime - startTime) * (1/CLOCK_RATE);;
 	time = (float)totalTime/(2*iterations);
 	cout << time  << endl;
 	return 0;
