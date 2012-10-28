6bee409fd258dd886b91eca85861b5956f672d27
Sumit Dhoble <sumit@vdeshpan-XPS-8300.(none)>
2012-10-28 10:46:37 -0700

    System call overhead

---
+++ SystemCallOverhead.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cstdio>
+#include"rdtsc.h"
+
+#define CLOCK_RATE 3.4
+
+using namespace std;
+
+void DoNothing(int i, int k, int l, int m) { };
+
+int main(){
+	int i, j, k, l, m, n, o, p;
+	bool comparison;
+	int iterations = 1000000;
+	int measurements = 100;
+//	struct timeval startTime, endTime, testTime, testTime2, testTime3;
+	int runtime;
+	float measurementResult = 0.0, finalResult = 0.0, sum = 0.0;
+	long int startTime, endTime;
+	FILE *fp;
+
+	fp = fopen("/dev/null","w");
+
+	for (i = 0; i < measurements; i++) {
+		startTime = rdtsc();
+		for (j = 0; j < iterations; j++) {
+			fprintf(fp,"%c",'a');
+		}
+		endTime = rdtsc();
+		runtime = endTime - startTime;
+		measurementResult = (float)runtime/iterations*(1/CLOCK_RATE);
+		cout << measurementResult << endl;
+	}
+	//finalResult = (float)sum/measurements;
+	//cout << finalResult << endl;
+	
+	return 0;
+}
