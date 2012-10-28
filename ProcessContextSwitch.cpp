03c4f3b514409e4a4c8266b6f2a1b9cc2f77a14a
Sumit Dhoble <sumit@vdeshpan-XPS-8300.(none)>
2012-10-28 12:29:54 -0700

    time reading for process context swtiching using rdtsc

--- ProcessContextSwitch.cpp
+++ ProcessContextSwitch.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<cstdlib>
-#include<sys/time.h>
 #include<unistd.h>
 #include<sys/wait.h>
 #include<cstdio>
+#include"rdtsc.h"
+
+#define CLOCK_RATE 3.4
 
 using namespace std;
 
@@ -23,10 +25,10 @@ int main(){
 
 	if(pid == 0){
 		int i = iterations, t1 = 0, time1;		
-		struct timeval startTime1, endTime1, startTime2, endTime2;
+		long int startTime1, endTime1, startTime2, endTime2;
 		char buf;
 
-		gettimeofday(&startTime1, NULL);
+		startTime1 = rdtsc();
 		for(i = iterations; i > 0; i--){
 			
 			close(pipePC[1]);
@@ -39,18 +41,18 @@ int main(){
 			close(pipeCP[1]);
 			//cout << "Written " << "h" << endl;
 		}
-		gettimeofday(&endTime1, NULL);
-		time1 = (endTime1.tv_sec - startTime1.tv_sec)*1000000 + (endTime1.tv_usec - startTime1.tv_usec);
+		endTime1 = rdtsc();
+		time1 = (endTime1 - startTime1) * (1 / CLOCK_RATE);
 		cout << (float)time1/(iterations) << endl;
 
 		exit(0);
 	}
 	else {
 		int i = iterations, t2 = 0, time2;
-		struct timeval startTime1, endTime1, startTime2, endTime2;
+		long int startTime1, endTime1, startTime2, endTime2;
 		char buf;
-
-		gettimeofday(&startTime2, NULL);
+	
+		startTime2 = rdtsc();
 		for(i = iterations; i > 0; i--){
 
 			close(pipePC[0]);          
@@ -64,8 +66,8 @@ int main(){
 			close(pipeCP[0]);
 			//cout << "Received "  << buf << endl;
 		}
-		gettimeofday(&endTime2, NULL);
-		time2 = (endTime2.tv_sec - startTime2.tv_sec)*1000000 + (endTime2.tv_usec - startTime2.tv_usec);
+		endTime2 = rdtsc();
+		time2 = (endTime2 - startTime2) * ( 1 / CLOCK_RATE );
 		cout <<  (float)time2/(iterations) << endl;
 
 
