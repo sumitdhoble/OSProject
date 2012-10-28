cfca4a45f87a57727ca1fe6c939ad0dae7904188
Sumit Dhoble <sumit@vdeshpan-XPS-8300.(none)>
2012-10-27 17:13:20 -0700

    Adding output files of loop overhead : empty and 1 argument

--- LoopOverhead.cpp
+++ LoopOverhead.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void doNothing(int i, int j, int k, int l, int m, int n, int o, int p){
+void doNothing(int i){
 }
 
 
@@ -18,13 +18,11 @@ int main(){
 	int runtime;
 	float measurementResult = 0.0, finalResult = 0.0, sum = 0.0;
 
-	cout << "hello" << endl;
-
 	for (i = 0; i < measurements; i++) {
 		gettimeofday(&startTime, NULL);
 		for (j = 0; j < iterations; j++) {
 			//gettimeofday(&testTime, NULL);
-			//doNothing(i, j, k, l, m, n, o, p);
+			doNothing(i);
 		}
 		gettimeofday(&endTime, NULL);
 		runtime = (endTime.tv_sec - startTime.tv_sec)*1000000 + (endTime.tv_usec - startTime.tv_usec);
@@ -32,8 +30,8 @@ int main(){
 		cout << measurementResult << endl;
 		sum += measurementResult;
 	}
-	finalResult = (float)sum/measurementResult;
-	cout << finalResult << endl;
+	//finalResult = (float)sum/measurements;
+	//cout << finalResult << endl;
 	
 	return 0;
 }
